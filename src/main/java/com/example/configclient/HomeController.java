25f9eae38fe2434ef37bc91c000ce0162cc660c8
Praveen Kumar <76509366+pkumar1AD@users.noreply.github.com>
2023-09-14 17:41:13 +0530

    Config Client Demo

---
+++ src/main/java/com/example/configclient/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.configclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HomeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/msg")
+    public String getWelcomeMessage(){
+        return environment.getProperty("welcome.message");
+    }
+
+}
